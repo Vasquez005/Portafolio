bae1c2313e3d74d75b9519e7e2b087b2c9300352
Armando Vásquez <3551680140101@ingenieria.usac.edu.gt>
2025-11-06 20:37:02 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var aboutMe = ["Full Stack Dev", "Computer Scientist", "Software Developer"];
+var p = document.querySelector(".about-me");
+var i = 0,
+  isAdding = true,
+  typed = 0;
+
+function typing() {
+  setTimeout(
+    function () {
+      p.innerText = aboutMe[typed].slice(0, i);
+      if (isAdding) {
+        if (i > aboutMe[typed].length) {
+          isAdding = false;
+          setTimeout(function () {
+            typing();
+          }, 2000);
+          return;
+        } else {
+          i++;
+        }
+      } else {
+        if (i === 0) {
+          isAdding = true;
+          typed = (typed + 1) % aboutMe.length;
+        } else {
+          i--;
+        }
+      }
+      typing();
+    },
+    isAdding ? 120 : 60
+  );
+}
+
+typing();
+
+AOS.init();
+
+window.addEventListener("load", function () {
+  AOS.refresh();
+});
